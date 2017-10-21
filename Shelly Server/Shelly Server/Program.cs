9244701d45993fb67728546ec07caaf1601ad56e
WeedmasterOG <31995172+WeedmasterOG@users.noreply.github.com>
2017-10-21 09:02:10 -0700

    Add files via upload

--- Shelly Server/Shelly Server/Program.cs
+++ Shelly Server/Shelly Server/Program.cs
@@ -1,88 +1,122 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Threading;
 
 namespace Shelly_Server
 {
     class Program
     {
+        // Server port
         public const string Ip = "127.0.0.1";
+
+        // Server port
         public const int Port = 8500;
+
 
         public static Thread disconnectionHandler = new Thread(DisconnectionHandler);
 
         static void Main(string[] args)
         {
+            NetworkTryAgain:
+
+            // Check network connectivity
+            try
+            {
+                // Create new instance
+                using (var Wc = new WebClient())
+                {
+                    // try to access google
+                    using (var stream = Wc.OpenRead("https://www.google.com"))
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+                // Show error message
+                ShowErrorMessage("ERROR: This program requires an active network connection to work", 5000);
+                goto NetworkTryAgain;
+            }
+
+            // Get SSL cert file path
             TryAgain:
             Console.WriteLine("SSL certificate file path");
 
             try
             {
+                // Get SSL cert file path
                 SocketHandler.CertPath = Console.ReadLine();
                 Console.Clear();
-                Console.WriteLine("SSL certificate file password");
 
-                var Cpass = "";
-                Cpass = Console.ReadLine();
-                SocketHandler.CertPass = Cpass;
+                // Get SSL cert file pass
+                Console.WriteLine("SSL certificate file password");
+                SocketHandler.CertPass = Console.ReadLine();
             } catch
             {
-                Console.WriteLine("ERROR: Could not find SSL certificate file");
-                Thread.Sleep(2000);
-                Console.Clear();
+                // Show error message
+                ShowErrorMessage("ERROR: Could not find SSL certificate file", 2000);
                 goto TryAgain;
             }
 
-
-
+            // Check if cert file exists
             if (!File.Exists(SocketHandler.CertPath))
             {
-                Console.WriteLine("ERROR: Could not find SSL certificate file");
-                Thread.Sleep(2000);
-                Console.Clear();
+                // Show error message
+                ShowErrorMessage("ERROR: Could not find SSL certificate file", 2000);
                 goto TryAgain;
             } else
             {
+                // Check if file is an actual cert file
                 if (Path.GetExtension(SocketHandler.CertPath) == ".pfx")
                 {
 
                 } else
                 {
-                    Console.WriteLine("ERROR: File is not an SSL certificate");
-                    Thread.Sleep(2000);
-                    Console.Clear();
+                    // Show error message
+                    ShowErrorMessage("ERROR: File is not an SSL certificate", 2000);
                     goto TryAgain;
                 }
             }
 
-
+            // Display message
+            Console.Clear();
             Console.WriteLine("Listening...");
 
+            // Start listening
             SocketHandler.Listen();
             Console.Clear();
 
+            // Accept incoming connection, authenticate.. etc
             SocketHandler.Connect();
 
+            // Display message
             Console.WriteLine("Client connected!");
             Thread.Sleep(2000);
             Console.Clear();
 
-
+            // Start the disconnection handler thread
             disconnectionHandler.Start();
 
+            // UserInput string
             string UserInput;
 
+            // Infinite loop
             while (true)
             {
+                // Take user input
                 Console.Write(">");
                 UserInput = Console.ReadLine();
                 Console.Write("\n");
 
+                // Try to read data
                 try
                 {
+                    // Turn to lower case and handle it
                     switch (UserInput.ToLower())
                     {
+                        // Shows help section
                         case "help":
                             Console.WriteLine(
                                 "1. Help - Displays this help section\n" + // None client
@@ -93,58 +127,99 @@ namespace Shelly_Server
                             );
                             break;
 
+                        // Clears the console
                         case "clear":
                             Console.Clear();
                             break;
 
+                        // Exit
                         case "exit":
+
+                            // Disconnect
                             SocketHandler.Disconnect();
+
+                            // Show disconnection message
                             ShowDisconnectMessage();
                             break;
 
+                        // Uninstall shell
                         case "uninstall":
+
+                            // Send message
                             SocketHandler.Send("uninstall");
+
+                            // Disconnect
                             SocketHandler.Disconnect();
+
+                            // Show disconnection message
                             ShowDisconnectMessage();
                             break;
-
+                        
+                        // Request Ping from client
                         case "ping":
+                            // Send message
                             SocketHandler.Send("ping");
+
+                            // Receive response
                             Console.WriteLine(SocketHandler.Receive());
                             break;
                     }
                 } catch
                 {
+                    // Disconnect
                     SocketHandler.Disconnect();
+
+                    // Show disconnection message
                     ShowDisconnectMessage();
                 }
             }
         }
 
+        // DisconnectionHandler thread
         public static void DisconnectionHandler()
         {
+            // Infinite loop
             while(true)
             {
+                // Check if connection is alive
                 if (SocketHandler.ConnectionStatus() == false)
                 {
+                    // 
                     SocketHandler.Disconnect();
 
+                    // Display message, NOTE: im not using the ShowDisconnectMessage method here as it messes with the main thread
                     Console.Clear();
                     Console.WriteLine("Client disconnected");
                     Thread.Sleep(2500);
+
+                    // Exit
                     Environment.Exit(0);
                 }
 
+                // Check every 5 seconds
                 Thread.Sleep(5000);
             }
         }
 
+        // ShowDisconnectMessage method
         public static void ShowDisconnectMessage()
         {
+            // Display message
             Console.Clear();
             Console.WriteLine("Client disconnected");
             Thread.Sleep(2500);
+
+            // Exit
             Environment.Exit(0);
+        }
+
+        // ShowErrorMessage method
+        public static void ShowErrorMessage(string Message, int Delay)
+        {
+            // Display message
+            Console.WriteLine(Message);
+            Thread.Sleep(Delay);
+            Console.Clear();
         }
     }
 }
--- Shelly Server/Shelly Server/SocketHandler.cs
+++ Shelly Server/Shelly Server/SocketHandler.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Net;
-using System.Net.Security;
+using System.Text;
 using System.Net.Sockets;
+using System.Net.Security;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 
 namespace Shelly_Server
 {
@@ -12,65 +12,95 @@ namespace Shelly_Server
     {
         public static string CertPath;
         public static string CertPass;
-        public static IPAddress localAdd = IPAddress.Parse(Program.Ip);
-        public static TcpListener listener = new TcpListener(localAdd, Program.Port);
-        public static TcpClient client;
-        public static byte[] Buffer;
-        public static SslStream sslStream;
+        private static IPAddress localAdd = IPAddress.Parse(Program.Ip);
+        private static TcpListener listener = new TcpListener(localAdd, Program.Port);
+        private static TcpClient client;
+        private static byte[] Buffer;
+        private static SslStream sslStream;
 
+        // Listen method
         public static void Listen()
         {
+            // Start listener
             listener.Start();
+
+            // Accept incoming connection(client)
             client = listener.AcceptTcpClient();
+
+            // Stop listener
             listener.Stop();
         }
 
+        // Connect method
         public static void Connect()
         {
+            // Create new sslstream instace, validate the certificate
             sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+
+            // Create new certificate instance
             X509Certificate2 cert = new X509Certificate2(CertPath, CertPass);
+
+            // Authenticate as server
             sslStream.AuthenticateAsServer(cert, false, SslProtocols.Tls, true);
+
+            // Set receive timeout
             sslStream.ReadTimeout = 10000;
         }
 
+        // ValidateServerCertificate method
         public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
             return true;
         }
 
+        // Disconnect method
         public static void Disconnect()
         {
+            // Stop the disconnectionHandler thread
             Program.disconnectionHandler.Abort();
+
+            // Close socket
             client.Close();
         }
 
+        // Send method
         public static void Send(string Message)
         {
+            // Write to the stream
             sslStream.Write(Encoding.ASCII.GetBytes(" " + Message), 0, Message.Length + 1);
         }
 
+        // Receive method
         private static string TrimSpace;
         public static string Receive()
         {
+            // Setup buffer
             Buffer = new byte[client.ReceiveBufferSize];
 
+            // Read incoming data and strip front space
             for (int i = 0; i < 2; i++)
             {
                 TrimSpace = Encoding.ASCII.GetString(Buffer, 0, sslStream.Read(Buffer, 0, Buffer.Length));
             }
 
+            // Return data received
             return TrimSpace;
         }
 
+        // ConnectionStatus method
         public static bool ConnectionStatus()
         {
+            // Try to send data to client
             try
             {
                 Send(".");
+
+                // Return true if operation succeeded
                 return true;
             }
             catch
             {
+                // Return false, connection is down
                 return false;
             }
         }
