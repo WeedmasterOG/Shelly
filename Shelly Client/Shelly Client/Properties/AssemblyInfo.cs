2c123e881aa507cb4feb2deaa6439a38d5679a12
WeedmasterOG <31995172+WeedmasterOG@users.noreply.github.com>
2017-10-24 15:09:34 -0700

    Add files via upload

--- Shelly Client/Shelly Client/Properties/AssemblyInfo.cs
+++ Shelly Client/Shelly Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shelly Client")]
-[assembly: AssemblyDescription("Shelly Client")]
+[assembly: AssemblyTitle("SC")]
+[assembly: AssemblyDescription("SC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Shelly Client")]
-[assembly: AssemblyProduct("Shelly Client")]
+[assembly: AssemblyCompany("SC")]
+[assembly: AssemblyProduct("SC")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
